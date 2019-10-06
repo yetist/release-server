ba407d9db5e128cf3237e5e845b0c275bc3d1d2e
Wu Xiaotian <yetist@gmail.com>
2019-10-06 12:30:29 +0800

    update to 0.1.5

--- config.go
+++ config.go
@@ -99,7 +99,7 @@ receivers = ["abc@example.com"]
 var config Config
 
 func init() {
-	LoadConfig("release-server", "0.1.4", "release-server.toml")
+	LoadConfig("release-server", "0.1.5", "release-server.toml")
 	if config.Security.ApiSecret == "" {
 		config.Security.ApiSecret = os.Getenv("API_SECRET")
 	}
